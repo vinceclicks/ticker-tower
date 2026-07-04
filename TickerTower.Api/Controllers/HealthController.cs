e5727020f4c20b16fa0a1985f64c598d37bc243d
vinceclicks <p.vincent.thomason@gmail.com>
2026-07-04 13:25:36 -0400

    Create api solution structure

---
+++ TickerTower.Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TickerTower.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "Healthy",
+            app = "TickerTower.Api",
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
